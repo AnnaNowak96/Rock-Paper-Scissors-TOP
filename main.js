b603804534212ce6291019d03415d36e50f3fdcc
AnnaNowak96 <jankowska.anna-96@wp.pl.noreply.github.com>
2024-01-08 16:01:34 +0100

    add UI feature

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let userScores = 0;
 let compScores = 0;
-const MaxTurns = 2;
+const MaxTurns = 5;
 
 
 //Wybor komuptera
@@ -8,7 +8,6 @@ let compChoice;
 
 function getComputerChoice() {
 let choice = Math.random() * 3;
-console.log(choice);
 if (choice <1){
     compChoice = "rock";
 }
@@ -22,41 +21,64 @@ console.log(compChoice);
 }
 
 
-//Logika gry
-function playRound() {
-const userChoice = prompt("Select your move: ").toLowerCase();
-if (userChoice == "rock" && compChoice == "scissors") {
+// Jaka klase ma nasz przycisk
+const btn = document.querySelector(".buttons");
+btn.addEventListener("click", (event) => {
+    const clickedButton = event.target;
+    console.log(clickedButton);
+    return clickedButton;
+});
+
+const rockBtn = document.getElementById("buttons-rock");
+const paperBtn = document.getElementById("buttons-paper");
+const scissorsBtn = document.getElementById("buttons-scissors");
+
+rockBtn.addEventListener("click", () => {
+    playRound("rock");
+})
+paperBtn.addEventListener("click", () => {
+    playRound("paper");
+})
+scissorsBtn.addEventListener("click", () => {
+    playRound("scissors");
+})
+
+function playRound(clickedButton) {
+getComputerChoice();
+if (clickedButton === "rock" && compChoice == "scissors") {
     userScores++;
-    alert("You won! Rock beats scissors.");
+    document.write("You won! Rock beats scissors.");
 } 
-else if (userChoice == "paper" && compChoice == "rock") {
+else if (clickedButton === "paper" && compChoice == "rock") {
     userScores ++;
     alert("You won! Paper beats rock.")
 }
-else if (userChoice == "scissors" && compChoice == "paper") {
+else if (clickedButton === "scissors" && compChoice == "paper") {
     userScores++;
     alert("You won! Scissors beat paper.");
 }
-else if (userChoice == compChoice) {
+else if (clickedButton === compChoice) {
     alert("Remis. No scores.");
 }
-else {
-    compScores++;
-    alert("Computer won! " + compChoice + " beats " + userChoice);
-}
-console.log(userChoice);
-console.log("Scores for user: " + userScores + " Scores for comp: " + compScores);
-}
 
 
-function gameplay() {
-do {
-    getComputerChoice();
-    playRound();
-    if (userScores >= MaxTurns || compScores >= MaxTurns) break;
-    }
-while (userScores < MaxTurns || compScores < MaxTurns);
+// else {
+//     compScores++;
+//     alert("Computer won! ");
+// }
+
+// console.log("Scores for user: " + userScores + " Scores for comp: " + compScores);
 }
 
+playRound();
 
-gameplay();
+// // function gameplay() {
+// // do {
+// //     getComputerChoice();
+// //     playRound();
+// //     if (userScores >= MaxTurns || compScores >= MaxTurns) break;
+// //     }
+// // while (userScores < MaxTurns || compScores < MaxTurns);
+// }
+
+
